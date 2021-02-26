c5bd515f94831df2f999111d9939262c8a03432a
KenChukanit <chonlasek.c@gmail.com>
2021-02-26 12:25:35 -0800

    setup rails and database

---
+++ biddr_api_rails/biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::AuctionsController < Api::ApplicationController
+    def index 
+     
+        auctions= Auction.order created_at: :desc
+        render json: auctions , each_serializer: AuctionSerializer
+       
+    end
+    def show
+        @auction=Auction.find params[:id]
+        render json: @auction
+    end
+    def create
+        auction = Auction.new auction_params
+        #auction.user=current_user
+        if auction.save
+            render json:{id: auction.id}
+        else
+            render(
+                json: {errors: auction.errors},
+                status: 422 
+            )
+        end
+    end
+
+    private
+    def auction_params
+        params.require(:auction).permit(:title,:description,:end_at,:reserve_price)
+    end
+end
