ed497ce7a2019ed01bcfe01f5d4fa29e9b96a66d
KenChukanit <chonlasek.c@gmail.com>
2021-02-26 12:38:28 -0800

    setup react

---
+++ biddr_client_react/biddr_client/src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React,{useState, useEffect} from 'react';
+import {NavLink} from 'react-router-dom';
+import './Navbar.css'
+
+function Navbar(props) {
+
+    function handleSignOut(){
+        props.destroySession();
+    }
+
+    return (
+       <nav className="navbar">
+        <div className= "navbar-container">
+
+        <NavLink className ="navbar-index"to='/' >
+            Menu 
+        </NavLink>
+        <NavLink className ="navbar-index"to='/auctions' >
+            Menu 
+        </NavLink>
+      
+            {
+                props.currentUser ? 
+                (
+                <div>
+                    <h3 className="nav-index">{props.currentUser.username}</h3> 
+                    
+                    <button className="btn--outline" onClick={handleSignOut}>Sign Out</button>
+                </div>
+                )
+                :(
+                <div>
+                    <NavLink className="navbar-cart" to='/'>
+                        Sign In
+                    </NavLink>
+    
+                    
+                </div>
+                )
+            }
+        </div>
+
+
+            
+        </nav>
+    )
+}
+
+export default Navbar
